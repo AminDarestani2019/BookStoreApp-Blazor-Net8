69fdaf8d9e23dad2f4bf046fad57215ee73ac223
Amin Darestani <darestani.amin@gmail.com>
2024-02-27 12:38:47 +0330

    the webAssembly app is created

---
+++ BookStoreApp.Blazor.WebAssembly.UI/Configuration/MapperConfig.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BookStoreApp.Blazor.WebAssembly.UI.Models.Author;
+using BookStoreApp.Blazor.WebAssembly.UI.Models.Book;
+
+namespace BookStoreApp.Blazor.WebAssembly.UI.Configuration
+{
+    public class MapperConfig:Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<AuthorDetailsDto, AuthorUpdateDto>().ReverseMap();
+            CreateMap<BookDetailsDto, BookUpdateDto>().ReverseMap();
+        }
+    }
+}
---
+++ BookStoreApp.Blazor.WebAssembly.UI/Data/Author.cs
@@ -0,0 +1,18 @@
+﻿namespace BookStoreApp.Blazor.WebAssembly.UI.Data;
+
+public partial class Author
+{
+    public Author()
+    {
+        Books = new HashSet<Book>();
+    }
+    public int Id { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public string? Bio { get; set; }
+
+    public virtual ICollection<Book> Books { get; set; }
+}
